32a67c9c4b5d560179859bb6d96fff2d6a111a09
Chris Matthias <chris.matthias@gmail.com>
2015-03-24 15:29:37 -0400

    transitioning to gulp/less for local asset development

---
+++ README.md
@@ -0,0 +1,16 @@
+### Dependencies
+
+* [Jekyll](http://jekyllrb.com)
+* [Node.js](http://nodejs.org)
+* [Bower](http://bower.io)
+
+### Dev
+
+1. Clone Repo into `~/yourprojectdir` (change yourprojectdir)
+2. Change into directory `cd ~/yourprojectdir`
+3. Install dependencies
+```
+bundle install && npm install && bower install
+```
+4. Run server: `bundle exec jekyll serve --watch`
+5. Compile LESS/JS with gulp: `gulp`
---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp =     require('gulp'),
+    less =     require('gulp-less'),
+    watch =    require('gulp-watch'),
+    concat =   require('gulp-concat'),
+    uglify =   require('gulp-uglifyjs'),
+    rename =   require('gulp-rename'),
+    cleancss = require('gulp-cleancss'),
+    path =     require('path'),
+    reload =   require('gulp-livereload');
+
+// Convert our LESS files to CSS
+gulp.task('less', function () {
+    gulp.src('./_src/less/app.less')
+        .pipe(less({
+            paths: [ path.join(__dirname, '_src', 'less', 'inc'), path.join(__dirname, 'bower_components') ]
+        }).on('error', function (err) {
+            console.log(err);
+        }))
+        .pipe(cleancss())
+        .pipe(rename('app.min.css'))
+        .pipe(gulp.dest('./assets/css'));
+});
+
+// Concatenate our app scripts
+
+gulp.task('scripts', function() {
+  return gulp.src([
+      './bower_components/jquery.scrollTo/jquery.scrollTo.js',
+      './_src/js/inc/shoreline-faux-input-autofill.js',
+      './_src/js/app.js'
+    ])
+    .pipe(uglify())
+    .pipe(concat('app.min.js'))
+    .pipe(gulp.dest('./assets/js/'));
+});
+
+// Watch our entire project for changes
+gulp.task('watch', function () {
+    // Watch our LESS files
+    gulp.watch('./_src/less/**/*.less', ['less']);
+    // Watch our script files
+    gulp.watch('./_src/js/**/*.js', ['scripts']);
+});
+
+// Watch our project by default
+gulp.task('default', ['watch']);
