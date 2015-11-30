cef2cb075ea0c47557b3f2a7fcd323d8d2a950a5
Chris Matthias <chris.matthias@gmail.com>
2015-11-30 10:54:54 -0500

    updating parse script locations to fix 401 error

--- _src/js/app.js
+++ _src/js/app.js
@@ -1,4 +1,1 @@
 // App Script
-
-// Initialize Parse with your Parse application & javascript keys
-Parse.initialize("xFxMfNpIfOjGD9pIFx7Q3jC0y8dSJgeJ8", "jhxWbls6wDXOiMO9ei5aj3NyFhPEbayjGRKmKsqm");
